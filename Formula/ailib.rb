7a6e7c4777a94c0892de96baa60fee090d1ada2b
Marcel de Jesus Silva <marcel@alisya.ai>
2026-04-27 04:18:30 +1000

    chore: update ailib formula to 1.0.8 (#2)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/ailib.rb
+++ Formula/ailib.rb
@@ -1,9 +1,9 @@
 class Ailib < Formula
   desc "Universal AI context-injection engine CLI"
   homepage "https://github.com/Alisya-AI/ai-lib"
-  url "https://registry.npmjs.org/@alisya.ai/ailib/-/ailib-1.0.2.tgz"
-  sha256 "944c4617fdf801dd5f5d61e9e413579e508193a17320e71b37f60d3cda7da43c"
-  version "1.0.2"
+  url "https://registry.npmjs.org/@alisya.ai/ailib/-/ailib-1.0.8.tgz"
+  sha256 "6105f4a4e88a7de69e1f02fea0891f166672d90941590d994303ba2d2b0854bb"
+  version "1.0.8"
   head "https://github.com/Alisya-AI/ai-lib.git", branch: "main"
   license "Apache-2.0"
 
